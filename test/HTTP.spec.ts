46041abe6cdda982b9345793d2cae1f1d8153f86
webistomin <webistomin@gmail.com>
2020-06-01 10:49:03 +0300

    test(http): fix failed test for http

--- test/HTTP.spec.ts
+++ test/HTTP.spec.ts
@@ -1,10 +1,11 @@
 import Nanogram from '../src/nanogram';
 import fetchMock from 'jest-fetch-mock';
+fetchMock.enableMocks();
 
 describe('Nanogram library', () => {
   describe('HTTP method', () => {
     let lib: Nanogram = null;
-    const URL = 'https://www.instagram.com/qwoepoqwpeoqpweoqw';
+    const URL = 'https://www.instagram.com/instagram';
 
     beforeEach(() => {
       fetchMock.resetMocks();
@@ -16,6 +17,7 @@ describe('Nanogram library', () => {
     });
 
     it("print error to console if can't parse content from string", async () => {
+      fetchMock.mockOnce('the next call to fetch will always return this as the body');
       const consoleSpy = jest.spyOn(console, 'error').mockImplementation();
       await lib['HTTP'](URL);
       expect(consoleSpy).toHaveBeenCalled();
