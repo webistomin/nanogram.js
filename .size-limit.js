48cce9b540b92d08bc021546449854f4232d6b6c
webistomin <webistomin@gmail.com>
2021-01-03 19:16:04 +0300

    build(size-limit): add single imports

--- .size-limit.js
+++ .size-limit.js
@@ -1,6 +1,61 @@
 module.exports = [
   {
     path: "dist/nanogram.esm.js",
-    limit: "5 KB",
-  }
+    name: "Total",
+    limit: "3 KB",
+  },
+  {
+    path: "dist/nanogram.esm.js",
+    name: "getCitiesByCountryId",
+    import: "{ getCitiesByCountryId }",
+    limit: "1.5 KB",
+  },
+  {
+    path: "dist/nanogram.esm.js",
+    name: "getCountries",
+    import: "{ getCountries }",
+    limit: "1.5 KB",
+  },
+  {
+    path: "dist/nanogram.esm.js",
+    name: "getMediaByLocation",
+    import: "{ getMediaByLocation }",
+    limit: "1.5 KB",
+  },
+  {
+    path: "dist/nanogram.esm.js",
+    name: "getMediaByPlaceId",
+    import: "{ getMediaByPlaceId }",
+    limit: "1.5 KB",
+  },
+  {
+    path: "dist/nanogram.esm.js",
+    name: "getMediaByPostId",
+    import: "{ getMediaByPostId }",
+    limit: "1.5 KB",
+  },
+  {
+    path: "dist/nanogram.esm.js",
+    name: "getMediaBySearchQuery",
+    import: "{ getMediaBySearchQuery }",
+    limit: "1.5 KB",
+  },
+  {
+    path: "dist/nanogram.esm.js",
+    name: "getMediaByTag",
+    import: "{ getMediaByTag }",
+    limit: "1.5 KB",
+  },
+  {
+    path: "dist/nanogram.esm.js",
+    name: "getMediaByUsername",
+    import: "{ getMediaByUsername }",
+    limit: "1.5 KB",
+  },
+  {
+    path: "dist/nanogram.esm.js",
+    name: "getPlacesByCityId",
+    import: "{ getPlacesByCityId }",
+    limit: "1.5 KB",
+  },
 ];
